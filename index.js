6d1aebc2c762a2592d749ae0147eae5fdfefb5ab
Claire <clairelogan16@gmail.com>
2021-07-06 10:51:54 -0700

    Foundational code

---
+++ index.js
@@ -0,0 +1,15 @@
+//fetch data and convert to JSON
+
+
+
+//Card Render
+function cardRender(card){
+    //code to render cards for each pokemon
+}
+
+
+//Initial Render
+function initialRender(){
+    //fetch data
+    //card render
+}

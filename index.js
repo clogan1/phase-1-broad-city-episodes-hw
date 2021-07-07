76f9b5fbe4488c3186fb58d6c7abcce0473713ea
Claire <clairelogan16@gmail.com>
2021-07-06 18:40:22 -0700

    finished project

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 //fetch data and convert to JSON
 
+fetch('http://api.tvmaze.com/shows/375/episodes')
+.then(res => res.json())
+.then(json => json.forEach(cardRender))
+
+//Card Render Function
+function cardRender(episodeData){
+    //creating elements
+    let divContainer = document.createElement('div')
+    let divContent = document.createElement('div')
+    //let divImage = document.createElement('div')
+    let h3Title = document.createElement('h3')
+    let pSeason = document.createElement('p')
+    let img = document.createElement('img')
+
+    //adding identification to elements
+    divContainer.className= 'episodeCard'
+    divContent.className = 'episodeContent'
+
+    //adding content to elements
+    img.src = episodeData.image.original
+    //console.log(episodeData.image.original)
+    h3Title.textContent = episodeData.name
+    pSeason.textContent = "Season: " + episodeData.season + " | Episode: " + episodeData.number
 
 
-//Card Render
-function cardRender(card){
-    //code to render cards for each pokemon
+    //appending elements to the DOM
+    divContent.append(img, h3Title, pSeason)
+    divContainer.append(divContent)
+    document.querySelector("#allCardContainer").append(divContainer)
+
+    divContent.addEventListener('click', () => showSummary(episodeData.summary))
 }
 
-
-//Initial Render
-function initialRender(){
-    //fetch data
-    //card render
+//showSummary callback function for event listner
+function showSummary(summary){
+    console.log(summary);
 }
+
